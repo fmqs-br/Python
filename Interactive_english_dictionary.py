647dc626abf96c4370268bb44bbc44554ce77ebb
Felipe <felipe.marques012@gmail.com>
2020-05-25 18:56:53 -0300

    Defining user input and function to the dictionary

--- Interactive_english_dictionary.py
+++ Interactive_english_dictionary.py
@@ -1,1 +1,11 @@
-print('hello')
+import json
+
+data = json.load(open("Dictionary\data.json"))
+
+def dictionary(word):
+    return data[word]
+
+word = input("Please, enter the word:>>")
+
+print(dictionary(word))
+
