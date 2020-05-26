eb571085e301ab924b8cacfee145eb5def6749b6
Felipe <felipe.marques012@gmail.com>
2020-05-26 18:53:53 -0300

    Reccomending the best match

--- Dictionary/Interactive_english_dictionary.py
+++ Dictionary/Interactive_english_dictionary.py
@@ -1,15 +1,20 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("Dictionary\data.json"))
 
 def dictionary(w):
+    w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys(), cutoff=0.8)) > 0:
+        return "Did you mean %s instead?" % get_close_matches(w, data.keys(), cutoff=0.8)[0]
     else:
         return "Sorry, the expression is not in the dictionary. Please, search for any mistake."
 
 word = input("Please, enter the word:>>")
-print(dictionary(word))
+
+print(dictionary(word))    
        
     
 
