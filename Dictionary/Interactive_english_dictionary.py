acf3f390194911fb929773270d33167648cc3187
Felipe <felipe.marques012@gmail.com>
2020-05-26 17:30:26 -0300

    Taking in account wrong words

--- Dictionary/Interactive_english_dictionary.py
+++ Dictionary/Interactive_english_dictionary.py
@@ -2,10 +2,15 @@ import json
 
 data = json.load(open("Dictionary\data.json"))
 
-def dictionary(word):
-    return data[word]
+def dictionary(w):
+    if w in data:
+        return data[w]
+    else:
+        return "Sorry, the expression is not in the dictionary. Please, search for any mistake."
 
 word = input("Please, enter the word:>>")
-
 print(dictionary(word))
+       
+    
+
 
