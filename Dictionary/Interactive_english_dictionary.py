19df2830057685c0555dce42cd576f6400ee9919
Felipe <felipe.marques012@gmail.com>
2020-05-27 17:57:07 -0300

    Confirmation from the user

--- Dictionary/Interactive_english_dictionary.py
+++ Dictionary/Interactive_english_dictionary.py
@@ -8,14 +8,17 @@ def dictionary(w):
     if w in data:
         return data[w]
     elif len(get_close_matches(w, data.keys(), cutoff=0.8)) > 0:
-        return "Did you mean %s instead?" % get_close_matches(w, data.keys(), cutoff=0.8)[0]
+        yn = input("Did you mean %s instead? Enter \"Y\" if Yes, or \"N\" if no:>>"  % get_close_matches(w, data.keys(), cutoff=0.8)[0])
+        yn = yn.upper()
+        if yn == 'Y':
+            return data[get_close_matches(w, data.keys(), cutoff=0.8)[0]]
+        elif yn == 'N':
+            return "Sorry, the expression is not in the dictionary. Please, search for any mistake."
+        else:
+            return "Sorry, we did not understand your entry."
     else:
         return "Sorry, the expression is not in the dictionary. Please, search for any mistake."
 
 word = input("Please, enter the word:>>")
 
-print(dictionary(word))    
-       
-    
-
-
+print(dictionary(word))
