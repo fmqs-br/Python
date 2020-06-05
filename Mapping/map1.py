d6b2c8cff977eadaebbd93adc8f288861f1a47c7
Felipe <felipe.marques012@gmail.com>
2020-06-05 14:47:55 -0300

    Map with popup and message

--- Mapping/map1.py
+++ Mapping/map1.py
@@ -1,11 +1,23 @@
 import folium
 import pandas
 
-map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 data = pandas.read_csv("volcanoes.csv")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+html = """<h4>Volcano information:</h4>
+Height: %s m
+"""
+
+map = folium.Map(location=[38.58, -99.09], min_zoom=3, zoom_start= 6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[38.58, -99.09], popup="Hi, I am a marker", icon=folium.Icon(color='green')))
+
+for lt, ln, el in zip(lat, lon, elev):
+    iframe = folium.IFrame(html=html % str(el), width=200, height=100)
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='green')))
+
 map.add_child(fg)
 
 map.save("Map1.html")
