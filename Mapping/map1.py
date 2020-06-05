0bd6be543e59bab6e46ad9d9d632cfbc3c1ff12d
Felipe <felipe.marques012@gmail.com>
2020-06-05 15:49:39 -0300

    Changing popup colors

--- Mapping/map1.py
+++ Mapping/map1.py
@@ -13,13 +13,21 @@ Volcano name:<br>
 Height: %s m
 """
 
+def color_popup(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000 <= elevation < 3000:
+        return "orange"
+    else:
+        return "red"
+
 map = folium.Map(location=[38.58, -99.09], min_zoom=3, zoom_start= 6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_popup(el))))
 
 map.add_child(fg)
 
